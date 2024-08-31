b0a7d17bcfe2a0df5cac93ded686ca834aff23a4
Vineet Kumar <135224728+Vineet153015@users.noreply.github.com>
2024-09-01 01:16:14 +0530

    UserAuth completed

--- lib/Screens/userAuth.dart
+++ lib/Screens/userAuth.dart
@@ -1,14 +1,78 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class userAuth extends StatelessWidget{
+class userAuth extends StatefulWidget {
+  @override
+  State<userAuth> createState() => _userAuthState();
+}
+
+class _userAuthState extends State<userAuth> {
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _meetingID = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login Page"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/logo.png"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _username,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      hintText: 'Enter your name',
+                      // Customize the border color
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0), // Border color when TextField is enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0), // Border color when TextField is focused
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    keyboardType: TextInputType.name,
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  TextField(
+                    controller: _username,
+                    decoration: InputDecoration(
+                      labelText: 'Meeting ID',
+                      hintText: 'Meet ID',
+                      // Customize the border color
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0), // Border color when TextField is enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0), // Border color when TextField is focused
+                      ),
+                      prefixIcon: Icon(Icons.numbers_outlined),
+                    ),
+                    keyboardType: TextInputType.name,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      String userName = _username.text;
+                      print('User Name: $userName');
+                    },
+                    child: Text('Join Meet'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
